9be9e86d6ce4bfb4763afbbbaa991f7b3c506fe0
MrDulfin <mrdulfin@dangoware.org>
2025-04-12 18:34:44 -0400

    implemented custom dimensions. `smallest` option doesn't currently function properly

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use std::{error::Error, fmt::Debug, fs::{self, OpenOptions}, io::Read, path::PathBuf};
+use std::{
+    error::Error,
+    fmt::Debug,
+    fs::{self, OpenOptions},
+    io::Read,
+    path::PathBuf,
+};
 
-use clap::{value_parser, Arg, ArgAction, ArgMatches, Command};
+use clap::{Arg, ArgAction, ArgMatches, Command, value_parser};
 use ffmpeg_sidecar::{
     command::{FfmpegCommand, ffmpeg_is_installed},
     ffprobe::ffprobe_is_installed,
 };
 use ffprobe::{FfProbeError, ffprobe};
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -26,21 +33,24 @@ fn main() -> Result<(), Box<dyn Error>> {
         if let Some(dir) = defaults.get_one::<PathBuf>("output_directory") {
             config.out_dir = Some(dir.clone())
         }
-        if let Some(dir) = defaults.get_one::<Dimensions>("dimensions") {
-            config.dimensions = Some(dir.clone())
+        if let Some(dim) = defaults.get_one::<Dimensions>("dimensions") {
+            if let Dimensions::Smallest = dim {
+                return Err("The \"smallest\" dimensions option is currently unsupported".into());
+            }
+            config.dimensions = Some(dim.clone())
         }
 
         match OpenOptions::new()
-        .read(true)
-        .write(true)
-        .truncate(true)
-        .open(&config.file)
+            .read(true)
+            .write(true)
+            .truncate(true)
+            .open(&config.file)
         {
             Ok(file) => {
                 serde_json::to_writer(file, &config)?;
                 println!("Config successfully updated");
             }
-            Err(e) => return Err(e.into())
+            Err(e) => return Err(e.into()),
         }
         return Ok(());
     }
@@ -48,9 +58,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let out_file = if let Some(path) = matches.get_one::<PathBuf>("output") {
         path.to_path_buf()
     } else {
-        config.out_dir.map(|dir| dir.join("./output.mp4")).unwrap_or_else(|| {
-            PathBuf::from("./output.mp4")
-        })
+        config
+            .out_dir
+            .map(|dir| dir.join("./output.mp4"))
+            .unwrap_or_else(|| PathBuf::from("./output.mp4"))
     };
 
     fn input(command: &mut FfmpegCommand, inputs: &mut Vec<InputFile>, path: PathBuf) {
@@ -65,7 +76,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .extension()
                 .is_some_and(|ext| ext.eq("mp4") || ext.eq("mkv") || ext.eq("mov"))
             {
-                let input = check_for_tracks(&path).unwrap();
+                let input = ffprobe_tracks(&path).unwrap();
                 // We skip "reserved" colour space because I don't know how to handle it right now
                 if input.colour_space.as_str() != "reserved" {
                     command.input(path.to_str().unwrap());
@@ -76,11 +87,41 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     let mut command = FfmpegCommand::new();
-    let paths= matches.get_many::<PathBuf>("input").unwrap();
+    let paths = matches.get_many::<PathBuf>("input").unwrap();
     let mut inputs = vec![];
     for path in paths {
         input(&mut command, &mut inputs, path.to_path_buf());
     }
+
+    let (x, y) = {
+        let dimensions = if let Some(d) = matches.get_one::<Dimensions>("dimensions") {
+            d
+        } else {
+            config.dimensions.as_ref().unwrap_or(&Dimensions::Largest)
+        };
+        match dimensions {
+            Dimensions::Largest => {
+                inputs.iter().by_ref().sorted_by(|a, b| {
+                    (a.width*a.height).cmp(&(b.width*b.height))
+                })
+                .map(|i| (i.width, i.height))
+                .nth(0)
+                .unwrap()
+            }
+            Dimensions::Smallest => {
+                // inputs.iter().by_ref().sorted_by(|a, b| {
+                //     (a.width*a.height).cmp(&(b.width*b.height))
+                // })
+                // .map(|i| (i.width, i.height))
+                // .last()
+                // .unwrap()
+                return Err("The \"smallest\" dimensions option is currently unsupported".into());
+            }
+            Dimensions::Px { x, y } => {
+                (x.clone(),y.clone())
+            }
+        }
+    };
 
     command.args("-f lavfi -i anullsrc=channel_layout=stereo".split(' '));
     let len = inputs.len();
@@ -94,7 +135,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     for (i, input) in inputs.iter().enumerate() {
         let var = num2words::Num2Words::new(i as i32).to_words().unwrap();
         let InputFile { audio, .. } = input;
-        filter_string.push_str(format!("[{i}:v]scale=1920:1080:force_original_aspect_ratio=decrease,setdar=ratio=16/9,setsar=sar=1/1,pad=ih*16/9/sar:ih:(ow-iw)/2:(oh-ih)/2[{var}];").as_str());
+        filter_string.push_str(format!("[{i}:v]scale={x}:{y}:force_original_aspect_ratio=decrease,setdar=ratio={x}/{y},setsar=sar=1/1,pad=ih*{x}/{y}/sar:ih:(ow-iw)/2:(oh-ih)/2[{var}];").as_str());
         concat_string.push_str(format!("[{var}]").as_str());
         concat_string.push_str(
             match audio {
@@ -138,9 +179,11 @@ struct InputFile {
     video: usize,
     audio: Option<usize>,
     colour_space: String,
+    width: i64,
+    height: i64
 }
 
-fn check_for_tracks(path: impl AsRef<std::path::Path> + Debug) -> Result<InputFile, FfProbeError> {
+fn ffprobe_tracks(path: impl AsRef<std::path::Path> + Debug) -> Result<InputFile, FfProbeError> {
     let info = ffprobe(&path)?;
     let mut input = InputFile::default();
 
@@ -167,6 +210,12 @@ fn check_for_tracks(path: impl AsRef<std::path::Path> + Debug) -> Result<InputFi
         if let Some(colour_space) = stream.color_space {
             input.colour_space = colour_space;
         }
+        if let Some(width) = stream.width {
+            input.width = width;
+        }
+        if let Some(height) = stream.height {
+            input.height = height;
+        }
     }
 
     Ok(input)
@@ -192,7 +241,7 @@ fn clap() -> ArgMatches {
                 .value_parser(clap::value_parser!(PathBuf))
                 .action(ArgAction::Append)
                 .value_name("FILE or DIR")
-                .help("The input files/directories to concatenate together")
+                .help("The input files/directories to concatenate together"),
         )
         .arg(
             Arg::new("dimensions")
@@ -200,8 +249,7 @@ fn clap() -> ArgMatches {
                 .short('d')
                 .value_parser(parse_dimensions)
                 .action(ArgAction::Set)
-                .help("Set the dimensions of the output video")
-                .default_value("largest"),
+                .help("Set the dimensions of the output video"),
         )
         .arg(
             Arg::new("no_audio")
@@ -225,7 +273,7 @@ fn clap() -> ArgMatches {
                 .help("Print ffmpeg output"),
         )
         .subcommand(
-            Command::new("defaults")
+            Command::new("config")
                 .about("set the default input values")
                 .arg(
                     Arg::new("output_directory")
@@ -234,28 +282,35 @@ fn clap() -> ArgMatches {
                         .short('o')
                         .value_parser(value_parser!(PathBuf))
                         .action(ArgAction::Set)
-                        .help("Set the default output directory for your output file")
+                        .help("Set the default output directory for your output file"),
                 )
                 .arg(
                     Arg::new("no_audio")
                         .long("no_audio")
                         .short('n')
-                        .value_parser(value_parser!(bool))
+                        .value_parser(value_parser!(bool)),
                 )
-                .arg(Arg::new("overwrite").long("overwrite").short('y').value_parser(value_parser!(bool)))
-                .arg(Arg::new("dimensions").long("dimensions").short('d').value_parser(parse_dimensions))
+                .arg(
+                    Arg::new("overwrite")
+                        .long("overwrite")
+                        .short('y')
+                        .value_parser(value_parser!(bool)),
+                )
+                .arg(
+                    Arg::new("dimensions")
+                        .long("dimensions")
+                        .short('d')
+                        .value_parser(parse_dimensions),
+                ),
         )
         .get_matches()
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 enum Dimensions {
-    Px {
-        x: usize,
-        y: usize
-    },
+    Px { x: i64, y: i64 },
     Largest,
-    Smallest
+    Smallest,
 }
 
 fn parse_dimensions(dimensions: &str) -> Result<Dimensions, String> {
@@ -264,8 +319,8 @@ fn parse_dimensions(dimensions: &str) -> Result<Dimensions, String> {
         "smallest" | "s" => Ok(Dimensions::Smallest),
         _ => {
             let mut dimensions = dimensions.split(':');
-            let x = dimensions.nth(0).unwrap().parse::<usize>().unwrap();
-            let y = dimensions.nth(0).unwrap().parse::<usize>().unwrap();
+            let x = dimensions.nth(0).unwrap().parse::<i64>().unwrap();
+            let y = dimensions.nth(0).unwrap().parse::<i64>().unwrap();
             Ok(Dimensions::Px { x, y })
         }
     }
@@ -286,8 +341,7 @@ impl Defaults {
         if let Some(dirs) = directories::ProjectDirs::from("moe", "mrdulfin", "memmet") {
             let config_dir = dirs.config_dir();
 
-            fs::create_dir_all(config_dir)
-            .or_else(|err| {
+            fs::create_dir_all(config_dir).or_else(|err| {
                 if err.kind() == std::io::ErrorKind::AlreadyExists {
                     Ok(())
                 } else {
@@ -302,20 +356,21 @@ impl Defaults {
                 .write(true)
                 .open(path.clone())
             {
-                serde_json::from_reader(&file).or_else(|e| {
-                    if e.is_eof() {
-                        Ok(Defaults::default())
-                    } else {
-                        Err(e.into())
-                    }
-                }).map(|mut d| {
-                    d.file = path;
-                    d
-                })
+                serde_json::from_reader(&file)
+                    .or_else(|e| {
+                        if e.is_eof() {
+                            Ok(Defaults::default())
+                        } else {
+                            Err(e.into())
+                        }
+                    })
+                    .map(|mut d| {
+                        d.file = path;
+                        d
+                    })
             } else {
                 Ok(Defaults::default())
             }
-
         } else {
             todo!()
         }
